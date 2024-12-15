da01cb0143e95f39a573f8178a412c1c657054bf
Aleksandr Yolshin <yolshin2009@yandex.ru>
2024-12-15 19:52:32 +0300

    3 задача готова

--- R4C/settings.py
+++ R4C/settings.py
@@ -123,3 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
