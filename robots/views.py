49a2db0bdae8a52b307cccfffe7bc84a3ad81fa4
Aleksandr Yolshin <yolshin2009@yandex.ru>
2024-12-15 21:18:12 +0300

    2 задача готова

--- robots/views.py
+++ robots/views.py
@@ -1,30 +1,11 @@
-import mimetypes
 import os
 from django.conf import settings
-from django.db.models import Count
-
-from django.http import HttpResponse, FileResponse
-from django.shortcuts import render
-from openpyxl.workbook import Workbook
-
+from django.http import FileResponse
 from robots.excel_report import robots_report
-from robots.models import Robot
 
 
 def download_file(request):
-    # fill these variables with real values
-    # fl_path = os.getcwd() + '\\robots\Book.xlsx'
-    # filename = 'Book.xlsx'
-    #
-    # fl = open(fl_path, 'r')
-    # mime_type, _ = mimetypes.guess_type(fl_path)
-    # response = HttpResponse(fl, content_type=mime_type)
-    # response['Content-Disposition'] = 'attachment; filename=%s' % filename
-    # return response
-
     robots_report()
     file = os.path.join(settings.BASE_DIR, 'report.xlsx')
     flopen = open(file, 'rb')
     return FileResponse(flopen)
-
-
